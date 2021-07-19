f16daf6229267b4c8b113664466dfa8a4b5791d7
HarveyPeachey <harveypeachey@hotmail.co.uk>
2021-07-20 00:06:03 +0100

    removed create-react-app header images and added header from wireframe

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,21 +1,9 @@
-import logo from '../logo.svg';
 import './Header.css';
 
 const Header = () => {
   return (
     <header className="Header">
-      <img src={logo} className="Header-logo" alt="logo" />
-      <p>
-        Edit <code>src/Header/Header.js</code> and save to reload.
-      </p>
-      <a
-        className="Header-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+      <h1>Electric Guitar Image Classifier</h1>
     </header>
   );
 }
