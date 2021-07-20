1ffb868e61f25f7ca5421390955760d86eb6c704
HarveyPeachey <harveypeachey@hotmail.co.uk>
2021-07-20 12:48:13 +0100

    added image upload functionality

--- src/ImageUploader/ImageUploader.js
+++ src/ImageUploader/ImageUploader.js
@@ -1,13 +1,35 @@
+import {useEffect, useState} from 'react';
 import './ImageUploader.css';
 import Button from '../Button/Button'
+import {useDropzone} from 'react-dropzone';
 
 const ImageUploader = () => {
+  const [image, setImage] = useState([]);
+  const {getRootProps, getInputProps} = useDropzone({
+    accept: 'image/jpeg, image/png',
+    maxFiles: 1,
+    onDrop: acceptedFiles => {
+      setImage( Object.assign(acceptedFiles[0], {preview: URL.createObjectURL(acceptedFiles[0])}));
+    }
+  });
+
+  const ImagePreview = () => (<img src={image.preview} alt=''/>);
+
+  useEffect(() => () => {
+    URL.revokeObjectURL(image.preview);
+  }, [image]);
+
   return (
+    <>
+    <ImagePreview/>
     <form>
-     <label for="img">Select image:</label>
-     <input type="file" id="img" name="img" accept="image/*" />
+      <div {...getRootProps({className: `${image.length === 0 ? 'dropzone' : 'dropzone-hidden'}`})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop your image here, or click to select an image</p>
+      </div>
      <Button text="Commune"/>
     </form>
+    </>
   );
 }
 
