66a77e336ed9c5730c92f538a9c0ff0cc94578fd
HarveyPeachey <harveypeachey@hotmail.co.uk>
2021-07-20 23:47:41 +0100

    prints the posted image file

--- api/api.py
+++ api/api.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 import time
 
 app = Flask(__name__)
 
-@app.route("/classify")
+@app.route("/classify", methods=['POST'])
 def classify():
+    print(request.files['file'])
     time.sleep(5)
     return {'name': 'Fender Stratocaster'}
